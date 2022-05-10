359fd252c39a9e086874246b753aa5ea39b02d91
Hassan Hammad <hassan@Hassans-MacBook-Pro.local>
2022-05-10 10:10:42 +0500

    added feedback widgets

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,77 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:teamway_demo_quiz/utilities/app_config.dart';
 import 'package:teamway_demo_quiz/utilities/constants.dart';
+import 'package:teamway_demo_quiz/views/common_widgets/feedback_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    FeedbackWidgets().setRootContext(context);
     AppConfig().init(context);
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: AppConfig.safeAreaTop,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-          const Expanded(child: SizedBox()),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              'Are you an introvert or an extrovert?',
-              textAlign: TextAlign.left,
-              style: AppConfig.getTextStyle(
-                context: context,
-                textColor: TextColor.primary,
-                textSize: TextSize.main,
-                fontWeight: FontWeight.bold,
-              ),
+    return WillPopScope(
+      onWillPop: () async {
+        return await FeedbackWidgets.showConfirmationDialog(
+          title: 'Quit?',
+          message: 'Are you sure you want to quit?',
+        );
+      },
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: AppConfig.safeAreaTop,
+              color: Theme.of(context).colorScheme.primary,
             ),
-          ),
-          const Expanded(flex: 1, child: SizedBox()),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              'Learn more about your psychology. Take the quiz to find out if you are an introvert or an extrovert.',
-              textAlign: TextAlign.left,
-              style: AppConfig.getTextStyle(
-                context: context,
-                textColor: TextColor.primaryLight,
-                textSize: TextSize.large,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-          const Expanded(
-            flex: 3,
-            child: SizedBox(),
-          ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10,
-                ),
-              ),
-              onPressed: () {},
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Text(
-                'Start Quiz',
+                'Are you an introvert or an extrovert?',
+                textAlign: TextAlign.left,
                 style: AppConfig.getTextStyle(
                   context: context,
-                  textSize: TextSize.normal,
+                  textColor: TextColor.primary,
+                  textSize: TextSize.main,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          const Expanded(child: SizedBox()),
-        ],
+            const Expanded(flex: 1, child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                'Learn more about your psychology. Take the quiz to find out if you are an introvert or an extrovert.',
+                textAlign: TextAlign.left,
+                style: AppConfig.getTextStyle(
+                  context: context,
+                  textColor: TextColor.primaryLight,
+                  textSize: TextSize.large,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 3,
+              child: SizedBox(),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Start Quiz',
+                  style: AppConfig.getTextStyle(
+                    context: context,
+                    textSize: TextSize.normal,
+                  ),
+                ),
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+          ],
+        ),
       ),
     );
   }
